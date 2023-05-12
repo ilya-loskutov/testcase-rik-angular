149fae7c6a3f4ebb6ce3f25a0bf4e2aed5689900
Ilya Loskutov <i.s.loskutov@yandex.com>
2023-05-12 14:18:50 +0300

    Add base markup for the account actions component

--- src/app/components/account-actions/account-actions.component.ts
+++ src/app/components/account-actions/account-actions.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
+
 @Component({
   selector: 'app-account-actions',
   templateUrl: './account-actions.component.html',
   styleUrls: ['./account-actions.component.scss']
 })
 export class AccountActionsComponent {
-
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon('arrow-left', sanitizer.bypassSecurityTrustResourceUrl('./assets/icons/arrow-left.svg'));
+  }
 }
