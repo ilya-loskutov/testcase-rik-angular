bcd01c483b3a8d3c81d399b106ba4b846a0640e4
Ilya Loskutov <i.s.loskutov@yandex.com>
2023-05-14 12:20:57 +0300

    Create user servers and models

---
+++ src/app/models/user.ts
@@ -0,0 +1,39 @@
+export class User {
+    constructor(
+        public readonly id: number,
+        public readonly name: string,
+        public readonly email: string,
+        public readonly phoneNumber: number,
+        public readonly creationDate: Date,
+        public readonly updatedDate: Date,
+        public readonly role: 'user' | 'admin',
+        public status: 'active' | 'blocked',
+        public readonly hasDigitalSignature: boolean
+    ) { }
+}
+
+export type ServerUser = {
+    readonly id: number,
+    readonly name: string,
+    readonly email: string,
+    readonly phone: number,
+    readonly create_at: number,
+    readonly update_at: number,
+}
+
+export type ServerUserData = {
+    readonly user_id: number,
+    readonly is_admin: boolean,
+    readonly is_ecp: boolean,
+    readonly status: 'ACTIVE' | 'BLOCKED'
+}
+
+export type ServerUserPage = {
+    readonly page: {
+        readonly total: number,
+        readonly current: number,
+        readonly size: number
+    },
+    readonly users: ServerUser[],
+    readonly data: ServerUserData[]
+}
